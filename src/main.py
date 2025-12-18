de227cdb2768cdad428e364866675ae176ec53b2
NicolasRidoire <nridoire@gmail.com>
2025-12-18 10:14:32 +0100

    test cam

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 from flask import Flask, request
 from command import execCommand
+import cv2
 
 app = Flask(__name__)
+cam = cv2.VideoCapture(0)
 
 @app.route("/")
 def home():
     content = "Page not found"
     with open('./page/home.html') as file:
         content = file.read()
+        ret, frame = cam.read()
+        cv2.imshow('Camera', frame)
     return content
 
 @app.route("/command", methods=["POST"])
