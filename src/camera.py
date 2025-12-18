a5fde3a4b8b964399cd580ad8fe603b1abb2f0cb
NicolasRidoire <nridoire@gmail.com>
2025-12-18 11:06:00 +0100

    trying to fix camera

--- src/camera.py
+++ src/camera.py
@@ -1,9 +1,42 @@
 import cv2
 
-cam = cv2.VideoCapture(0, cv2.CAP_V4L2)
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1280,
+    capture_height=720,
+    display_width=640,
+    display_height=480,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "+
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "+
+        "nvvidconv flip-method=%d ! "+
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "+
+        "videoconvert ! "+
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+print("Initialisation GStreamer...")
+cam = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+
+if not cam.isOpened():
+    print("ERREUR: Impossible d'ouvrir la caméra avec GStreamer")
+else:
+    print("Caméra ouverte avec succès !")
+
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 def generateFrames():
     while True:
