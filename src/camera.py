096fa1a08f71d640531debc41f9e48eed5811bbf
NicolasRidoire <nridoire@gmail.com>
2025-12-18 11:13:30 +0100

    try again

--- src/camera.py
+++ src/camera.py
@@ -2,18 +2,11 @@ import cv2
 
 cam = cv2.VideoCapture(0, cv2.CAP_V4L2)
 
-if not cam.isOpened():
-    print("ERREUR: Impossible d'ouvrir la caméra avec GStreamer")
-else:
-    print("Caméra ouverte avec succès !")
-
 def generateFrames():
     while True:
         success, frame = cam.read()
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+        if success:
+            ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n' +
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
