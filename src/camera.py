560451b2d1e60a6b8e9cbd4bce328df0b53d3d8b
NicolasRidoire <nridoire@gmail.com>
2025-12-18 11:21:19 +0100

    again...

--- src/camera.py
+++ src/camera.py
@@ -6,7 +6,6 @@ def generateFrames():
     while True:
         success, frame = cam.read()
         if success:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
+            frame = frame.tobytes()
             yield (b'--frame\r\n' +
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
